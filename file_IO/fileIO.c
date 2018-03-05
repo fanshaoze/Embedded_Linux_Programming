61fbeba2f113d1689185167b7b5534eec019ece1
fanshaoze <fanshaoze123123@163.com>
2018-03-05 23:22:59 +0800

    open,read,write methods

--- file_IO/fileIO.c
+++ file_IO/fileIO.c
@@ -3,10 +3,19 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+#include <unistd.h>
+#include <errno.h>
 int main()
 {
     int fd;
+    unsigned long word;
+    ssize_t ret;
+    char buf[10];
+    char *buff = &word;
+    //extern int errno;
+    int len = 5;
+    char *p1 = "This is a c test code";
+    int i = 0;
     fd = open("test.txt",O_RDONLY);
     if (fd == -1)
     {
@@ -14,7 +23,51 @@ int main()
     }
     else
     {
-        printf("open file success\n");
+        printf("%d open file success\n",fd);
     }
+    ret = read(fd,buff,len);
+    while (len !=0 && ret !=0)
+    {
+    	printf("ret  %d\n",ret);
+    	printf("buf  %s\n",buff);
+    	if(ret == -1)
+    	{
+    		printf("wrong");
+    		if(errno == EINTR)
+    			continue;
+    		perror("read");
+    		break;
+    	}
+    	len -= ret;
+    	buff += ret;
+    	ret = read(fd,buff,len);
+
+
+    }
+    printf("buf  %s\n",buff);
+    int xd;
+    xd = creat("test2.txt",0666);
+    if (xd == -1)
+    {
+         printf("create file error\n");
+    }
+    else
+    {
+        printf("%d create file success\n",xd);
+    }
+    for(i = 0;i<21;i++)
+    {
+        	printf("%d   ",(strlen(p1)-len));
+         	int n;
+         	n=write(xd, p1+len, (strlen(p1)-len));
+         	printf("n = %d\n",n);
+         	if(n== 0 || n == -1)   //if((n=write(fp, p1+len, 3)) == 0)
+        {                                                 //strlen(p1) = 21
+             printf("n = %d \n", n);
+             break;
+        }
+        len+=n;
+    }
+
     return 0;
 }
