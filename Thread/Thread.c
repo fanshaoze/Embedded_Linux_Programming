ff27c2822f21869b49d33c96f59777e353f2c7e4
fanshaoze <fanshaoze123123@163.com>
2018-04-16 17:28:29 +0800

    cancel thread

--- Thread/Thread.c
+++ Thread/Thread.c
@@ -21,6 +21,7 @@ void* thr_fn(void *arg)
 {
     printf("new thread\n");
     return((void*)0);
+    //stop_thread();
 }
 
 int thread_eque(pthread_t thread1,pthread_t thread2){
@@ -36,7 +37,42 @@ int thread_eque(pthread_t thread1,pthread_t thread2){
 	}
 
 }
-int stop_thread(){
+int stop_thread(pthread_t thread){
+	int unused;
+	int ret;
+	ret = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE,&unused);
+	if(ret){
+		errno = ret;
+		perror("pthread_setcancelstate");
+		printf("pthread_setcancelstate fail\n");
+		return -1;
+	}
+	else{
+		printf("pthread_setcancelstate success\n");
+	}
+	ret = pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED,&unused);
+	if(ret){
+		errno = ret;
+		perror("pthread_setcanceltype");\
+		printf("pthread_setcanceltype fail\n");
+		return -1;
+	}
+	else{
+		printf("pthread_setcanceltype success\n");
+	}
+	printf("the thread will be stopped is %lu\n",thread);
+	ret = pthread_cancel(thread);//thread do another time???
+	if(ret){
+		errno = ret;
+		perror("pthread_cancel");
+		printf("pthread_cancel fail\n");
+		return -1;
+	}
+	else{
+		printf("pthread_cancel success\n");
+	}
+
+
 	return 0; 
 }
 
@@ -62,6 +98,6 @@ int main(){
 	pthread_t thread =1;
 	thread = create_thread(thread);
 	thread_eque(pthread_self(),thread);
-	stop_thread();
+	stop_thread(thread);
 	return 0;
 }
